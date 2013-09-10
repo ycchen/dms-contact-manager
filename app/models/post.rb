e4f380178d101e338f36cbd58d8c92916a355808
Yungchih Chen <yungchih_chen@yahoo.com>
2013-09-10 16:56:24 -0400

    Updated where condition to all lowercase on column name and search value

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,7 @@ class Post < ActiveRecord::Base
   def self.search(search)
   	if search
   		# find(:all, :joins => :contact, :conditions => ['posts.title LIKE ? or posts.body LIKE ? or contacts.first_name LIKE ? or contacts.last_name LIKE ? or contacts.company LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%" , "%#{search}%", "%#{search}%"])
-  		joins(:contact, :user).where(['posts.title LIKE ? or posts.body LIKE ? or contacts.first_name LIKE ? or contacts.last_name LIKE ? or contacts.company LIKE ? or users.display_name LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%" , "%#{search}%", "%#{search}%", "%#{search}%"]).order('created_at DESC')
+  		joins(:contact, :user).where(['LOWER(posts.title) LIKE ? or LOWER(posts.body) LIKE ? or LOWER(contacts.first_name) LIKE ? or LOWER(contacts.last_name) LIKE ? or LOWER(contacts.company) LIKE ? or LOWER(users.display_name) LIKE ?', "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%" , "%#{search.downcase}%", "%#{search.downcase}%", "%#{search.downcase}%"]).order('created_at DESC')
   	else
   		where.order('created_at DESC')
   	end
